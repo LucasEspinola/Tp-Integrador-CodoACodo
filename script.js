3b5eca000833ae080539c617ca5eed11973eab23
LucasEspinola <88916838+LucasEspinola@users.noreply.github.com>
2022-10-24 19:30:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let cantidad = document.getElementById("cant").value;
+let categoria = document.getElementById("cat").value;
+let elementoMod = document.getElementById("total");
+
+
+function calcularPrecio(){
+    cantidad = document.getElementById("cant").value;    
+    categoria = document.getElementById("cat").value;
+    elementoMod = document.getElementById("total");
+
+    if(categoria == 1){
+        elementoMod.innerHTML = ('Total a Pagar: $'+(cantidad * (200 * 0.8)));
+    }else if(categoria == 2){
+        elementoMod.innerHTML = ('Total a Pagar: $'+(cantidad * (200 * 0.5)));
+    }else{
+        elementoMod.innerHTML = ('Total a Pagar: $'+(cantidad * (200 * 0.15)));
+    }
+    
+}
+function borrarTodo(){
+    document.getElementById("mi-Form").reset();
+    elementoMod.innerHTML = ("Total a Pagar: $");
+}
+
